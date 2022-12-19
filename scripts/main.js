f72d4460a2f4bd6d6b43e142c7235a58ad4c3433
CelinexPutte <112859814+CelinexPutte@users.noreply.github.com>
2022-12-19 15:38:49 +0100

    Aanpassingen gedaan
    - Required toegevoegd
    - Zones toegevoegd
    - Submit button ipv input
    - Zoom op submit button door JS

--- scripts/main.js
+++ scripts/main.js
@@ -10,3 +10,30 @@ closeButton.addEventListener('click', menuToggle)
 function menuToggle() {
     nav.classList.toggle('visible')
 }
+
+// Foutmelding in input veld
+// Werkt nog niet
+// let invalid = document.querySelector('form')
+
+// invalid.addEventListener('invalid', errorColor)
+
+// function errorColor() {
+//     invalid.classList.add('input-invalid')
+// }
+
+
+// Zoom voor button
+let submit = document.querySelector('.submit-button')
+
+submit.addEventListener('mouseover', zoomToggle)
+submit.addEventListener('mouseout', zoomToggle)
+// menu.addEventListener('mouseover', zoomToggle)
+// menu.addEventListener('mouseout', zoomToggle)
+// closeButton.addEventListener('mouseover', zoomToggle)
+// closeButton.addEventListener('mouseout', zoomToggle)
+
+function zoomToggle() {
+    submit.classList.toggle('zoom-on-hover')
+    // menu.classList.toggle('zoom-on-hover')
+    // closeButton.classList.toggle('zoom-on-hover')
+}
